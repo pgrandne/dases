890da8dba98c2ece54e5930d6e1ca026c0dc104c
pgrandne <perrin.grandne@protonmail.com>
2022-01-07 00:30:48 +0100

    migration to React 5.0.0

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <p>Prototype du Portail de DASES</p>   
+        <p>
+          Prototype du Portail de DASES
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://www.notion.so/7e5c7a401b364686a37f4bbc962fdb55?v=a73ab8b5708740a79a212c8fb7e8a2a4" target="_blank" style="color:white">Voir la feuille de Route</a>
+        <a
+          className="App-link"
+          href="https://www.notion.so/7e5c7a401b364686a37f4bbc962fdb55?v=a73ab8b5708740a79a212c8fb7e8a2a4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir la feuille de route
+        </a>
       </header>
     </div>
   );
