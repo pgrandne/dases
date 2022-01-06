2a23e5a90bca15784a040241cde7af8cd2f4324c
Perrin <perrin.grandne@gmail.com>
2022-01-06 22:43:58 +0100

    Adding Roadmap

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+         <p>Prototype du Portail de DASES</p>   
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Prototype du Portail de DASES
-        </p>
+        <a href="https://www.notion.so/7e5c7a401b364686a37f4bbc962fdb55?v=a73ab8b5708740a79a212c8fb7e8a2a4">Voir la feuille de Route</a>
       </header>
     </div>
   );
