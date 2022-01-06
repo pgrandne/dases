632e75ea55fdc9dc016d2e34bebb04261266f30a
Perrin <perrin.grandne@gmail.com>
2022-01-06 21:30:40 +0100

    Proto Dases

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DID for DASES
+          Prototype du Portail de DASES
         </p>
       </header>
     </div>
