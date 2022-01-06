340adb495b0734522ae07d7a2146a974142fbd38
Perrin <perrin.grandne@gmail.com>
2022-01-06 22:48:59 +0100

    open the link in a new tab

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
          <p>Prototype du Portail de DASES</p>   
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://www.notion.so/7e5c7a401b364686a37f4bbc962fdb55?v=a73ab8b5708740a79a212c8fb7e8a2a4">Voir la feuille de Route</a>
+        <a href="https://www.notion.so/7e5c7a401b364686a37f4bbc962fdb55?v=a73ab8b5708740a79a212c8fb7e8a2a4" target="_blank">Voir la feuille de Route</a>
       </header>
     </div>
   );
